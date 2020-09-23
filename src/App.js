ad8bd55386a2eb0cf424804d3bcd51b05bd176e1
Guilherme Avelar <guilhermesantosavelar@gmail.com>
2020-09-23 11:34:12 -0300

    Store do redux configurada

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Provider} from 'react-redux';
 import './App.css';
+
+import store from './store';
 import SearchBar from './components/SearchBar';
 import VideoList from './components/VideoList';
 import VideoPlayer from './components/VideoPlayer';
 
 function App() {
   return (
-    <div className="container">
-      <SearchBar />
-      <VideoList />
-      <VideoPlayer />
-    </div>
+    <Provider store={store}>
+      <div className="container">
+        <SearchBar />
+        <VideoList />
+        <VideoPlayer />
+      </div>
+    </Provider>
   );
 }
 
