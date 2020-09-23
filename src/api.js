b78b742e064fb7ef60b5fce1e925324befc8280e
Guilherme Avelar <guilhermesantosavelar@gmail.com>
2020-09-23 10:59:29 -0300

    Primeiro commit e actions do redux

---
+++ src/api.js
@@ -0,0 +1,3 @@
+const API_KEY = 'AIzaSyBe2rCmb2c76y1dal8F50x7LDRadCET0O8'
+
+export default API_KEY;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import {Segment, Input} from 'semantic-ui-react';
+
+class SearchBar extends Component {
+
+    pesquisaTermo = e => {
+        if (e.keyCode === 13){
+            const termo = e.target.value
+            console.log(termo)
+        }
+    }
+
+    render() {
+        return (
+            <div className="search-bar">
+                <Segment stacked>
+                    <Input icon="search" size="large" placeholder="Search..." onKeyDown={(e) => this.pesquisaTermo(e)} />
+                </Segment>
+            </div>
+        )
+    }
+};
+
+export default SearchBar;
